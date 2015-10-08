3d2dde2907b619033f96c3083516020cfb9f8a67
luis <luis709394@gmail.com>
2015-10-08 18:53:42 -0400

    commit x

--- server.php
+++ server.php
@@ -9,7 +9,7 @@ $paths = require __DIR__.'/bootstrap/paths.php';
 $requested = $paths['public'].$uri;
 
 
-// Ths file  allows us to emulate Apache's "mod_rewrite" functionality from the
+// Ths file  allows us to emulate Apached's "mod_rewrite" functionality from the
 // Built-in PHP  server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
 if ($uri !== '/' and file_exists($requested))
